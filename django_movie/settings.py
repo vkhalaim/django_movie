1c8583a7cb9fabe211ed9c46ab327d6e7c4411e9
vkhalaim <vasyl.khalaim@ukr.net>
2020-08-25 18:19:08 +0300

    Adds layout.

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'django_movie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- movies/views.py
+++ movies/views.py
@@ -8,4 +8,4 @@ class MoviesView(View):
 
     def get(self, request):
         movies = Movie.objects.all()
-        return render(request, 'movies/movie_list.html', {"movie_list": movies})
+        return render(request, 'movies/movies.html', {"movie_list": movies})
