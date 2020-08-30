1d0197fc7dae91c9b7bec3155ed509cd9b29d614
vkhalaim <vasyl.khalaim@ukr.net>
2020-08-30 19:07:55 +0300

    Adds filter by genre/year.

--- movies/models.py
+++ movies/models.py
@@ -57,11 +57,19 @@ class Movie(models.Model):
     poster = models.ImageField("Poster", upload_to="movies/")
     year = models.PositiveSmallIntegerField("Release date", default=2020)
     country = models.CharField("Country", max_length=30)
-    directors = models.ManyToManyField(Actor, verbose_name="director", related_name="film_director")
-    actors = models.ManyToManyField(Actor, verbose_name="actors", related_name="film_actor")
+    directors = models.ManyToManyField(
+        Actor, verbose_name="director", related_name="film_director"
+        )
+    actors = models.ManyToManyField(
+        Actor, verbose_name="actors", related_name="film_actor"
+        )
     genres = models.ManyToManyField(Genre, verbose_name="genres")
-    world_premiere = models.DateField("Premiere in the world", default=date.today)
-    budget = models.PositiveIntegerField("Budget", default=0, help_text="in dollars")
+    world_premiere = models.DateField(
+        "Premiere in the world", default=date.today
+        )
+    budget = models.PositiveIntegerField(
+        "Budget", default=0, help_text="in dollars"
+        )
     fees_in_usa = models.PositiveIntegerField(
         "Fees in USA", default=0, help_text="in dollars"
     )
@@ -93,7 +101,9 @@ class MovieShots(models.Model):
     title = models.CharField("Title", max_length=100)
     description = models.TextField("Description")
     image = models.ImageField("Image", upload_to="movie_shots/")
-    movie = models.ForeignKey(Movie, verbose_name="Movie", on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie, verbose_name="Movie", on_delete=models.CASCADE
+        )
 
     def __str__(self):
         return self.title
@@ -118,8 +128,12 @@ class RatingStar(models.Model):
 class Rating(models.Model):
 
     ip = models.CharField("IP address", max_length=15)
-    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name="star")
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name="movie")
+    star = models.ForeignKey(
+        RatingStar, on_delete=models.CASCADE, verbose_name="star"
+        )
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, verbose_name="movie"
+        )
 
     def __str__(self):
         return f"{self.star} - {self.movie}"
@@ -135,9 +149,12 @@ class Reviews(models.Model):
     name = models.CharField("Name", max_length=100)
     text = models.TextField("Review", max_length=5000)
     parent = models.ForeignKey(
-        'self', verbose_name="Parent", on_delete=models.SET_NULL, blank=True, null=True
+        'self', verbose_name="Parent", on_delete=models.SET_NULL,
+        blank=True, null=True
     )
-    movie = models.ForeignKey(Movie, verbose_name='movie', on_delete=models.CASCADE)
+    movie = models.ForeignKey(
+        Movie, verbose_name='movie', on_delete=models.CASCADE
+        )
 
     def __str__(self):
         return f"{self.name} - {self.movie}"
--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,29 @@
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 from django.shortcuts import redirect
 
-from .models import Movie, Category, Actor
+from .models import Movie, Actor, Genre
 from .forms import ReviewForm
 
 
-class MoviesView(ListView):
+class GenreYear:
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
+
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    template_name = 'movies/movies_list.html'
+    template_name = 'movies/movie_list.html'
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
 
     model = Movie
     slug_field = 'url'
@@ -33,8 +44,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
 
     model = Actor
     template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
